0b6eedf0df906dc0ffc4e9bce56fa3b75ab8da45
muko10101 <69293445+muko10101@users.noreply.github.com>
2020-08-06 16:43:50 +0900

    Update sendcode.h

--- wio_MB/sendcode.h
+++ wio_MB/sendcode.h
@@ -1,7 +1,8 @@
-#define HITON  1
-#define HITOFF 0
+##define HITON  1  //打音観測した
+#define HITOFF 0  //打音観測しない
 
-void NowTempo_Text(void);
-void SendCode_Text(void);
-void SampleHits(void);
-void Send_Serial(void);
+void NowTempo_Text(void); //今のテンポを表示しているときのタイトル
+void SendCode_Text(void); //指令入力中のタイトル
+void SampleHits(void);    //指令入力
+void Send_Serial(void);   //PCに指令送信
+void LoopSend(void);      //断続的なPCへの指令送信
