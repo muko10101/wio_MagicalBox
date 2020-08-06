6242db68d3f9d1689009cb0d7648adc35959681d
muko10101 <69293445+muko10101@users.noreply.github.com>
2020-08-06 16:42:30 +0900

    Update hit.ino

--- wio_MB/hit.ino
+++ wio_MB/hit.ino
@@ -1,8 +1,8 @@
 #include  "hit.h"
 
 int Check_Hit() {
-  boolean hit = digitalRead(USIN);        // 現在のスイッチの状態を読み取る
-  if ( hit == SPN_HIT ) {        // 現在のスイッチがON
+  boolean hit = digitalRead(USIN);        // 超音波が発生したかどうかを読み取る
+  if ( hit == SPN_HIT ) {        // 超音波を観測
     return HITSOUND;
   }
   else {
