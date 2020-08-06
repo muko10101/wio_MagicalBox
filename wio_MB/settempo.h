5704e3cf32e29b69c872203695a8162c72851b54
muko10101 <69293445+muko10101@users.noreply.github.com>
2020-08-06 16:46:46 +0900

    Update settempo.h

--- wio_MB/settempo.h
+++ wio_MB/settempo.h
@@ -1,10 +1,9 @@
-void LastTempo_Text(void);
-void SetTempo_Text(void);
-void Tempo_Init(void);
-void Settempo_Notes(int num);
-void Sendcode_Notes(int num);
-unsigned long CheckTempo(void);
-//void Tempo_Disp(unsigned long interval);
-void Tempo_Disp(int tempover);
-unsigned long Measure(void);
-int RandamList(int num);
+void LastTempo_Text(void);     //今のテンポを表示しているときのタイトル
+void SetTempo_Text(void);      //テンポを入力しているときのタイトル
+void Tempo_Init(void);         //テンポ表示をはじめの位置からにする
+void Settempo_Notes(int num);  //テンポ入力前のテンポ表示画面の音符表示
+void Sendcode_Notes(int num);  //指令入力前のテンポ表示画面の音符表示
+unsigned long CheckTempo(void);//今の間隔時間（テンポ）を取得
+void Tempo_Disp(int tempover); //テンポ表示
+unsigned long Measure(void);   //テンポを設定する
+int RandamList(int num);       //ランダムで音符の色を変えるためにリスト内をランダムに入れ替える
